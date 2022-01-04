d20f64018e88e2f495c061e0c51a3f9ff7c3886a
reboot13-git <rautkrutik013@gmail.com>
2022-01-05 00:34:50 +0530

    updated files

---
+++ js/gsap.js
@@ -0,0 +1,43 @@
+let relPos="20px";
+
+autoCounter = document.getElementsByClassName("autoCounter")
+
+let span1 = autoCounter[0];
+let span2 = autoCounter[1];
+let span3 = autoCounter[2];
+function counter(el, limit) {
+    let i = limit - 100;
+    let element = el;
+    let interval = setInterval(() => {
+        el.innerText = i<=9 && i>=0 && i==0 ? "0"+Math.abs(i) +"+" : Math.abs(i) +"+";
+        i >= limit ? clearInterval(interval) : i++;
+
+    }, 16)
+}
+
+var checkCounter = true;
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.from(".about-head", { x: -100, opacity: 0, duration: 1, ease: Power2.easeOut });
+gsap.from(".about-para", { y: -100, opacity: 0, duration: 1, delay: 1, ease: Power2.easeOut });
+let animation=gsap.from(".trigger", {
+    scrollTrigger:
+    {
+        trigger: ".stats-div",
+        onEnter: function counterdiv() {
+            if(checkCounter){
+            counter(span1, 17);
+            counter(span2, 30000);
+            counter(span3, 7000);
+            }
+            checkCounter = false
+            
+        },
+        start: `${relPos} 80%`
+        
+       
+    },
+    x:"-20",
+    duration: 0.5,
+    stagger: 0.5
+});
